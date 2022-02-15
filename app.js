c895ecd2cb399d58064b5498e6f8f080f0e76a12
vandergj <vandergj@iu.edu>
2022-02-15 17:37:21 -0500

    finished

--- app.js
+++ app.js
@@ -47,3 +47,31 @@ let warriors = [
       warrior:false
     }
   ]
+
+for(let i=0; i<warriors.length; i++){
+  
+  if (warriors[i].health >= 10 && warriors[i].damage >= 2 && warriors[i].warrior){
+    
+    //make and add info to elements
+    let ele = document.createElement("div");
+    let name_ele = document.createElement('h1');
+    let damage_ele = document.createElement('h2');
+    let health_ele = document.createElement('h2');
+    name_ele.innerHTML=warriors[i].name;
+    damage_ele.innerHTML=warriors[i].damage;
+    health_ele.innerHTML=warriors[i].health;
+    //change color
+    if (warriors[i].name.includes('a')){
+      ele.style.color = 'red';
+    }    
+
+    //add elements to webpage
+    ele.appendChild(name_ele);
+    ele.appendChild(damage_ele);
+    ele.appendChild(health_ele);
+    document.body.appendChild(ele);
+
+  }
+  
+}
+
